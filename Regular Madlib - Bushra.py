7a35d68382a757f950d7c01991bd1e9831209a54
Bushara <56700757+Bushara-work@users.noreply.github.com>
2024-08-09 02:39:18 -0700

    Update Regular Madlib - Bushra.py

--- Regular Madlib - Bushra.py
+++ Regular Madlib - Bushra.py
@@ -5,13 +5,13 @@ import random
 Titles_and_Templates = {
     "Final Advertisement Script": "Are you looking for a {first adjective} {first noun}? Look no further! Our {first adjective} {first noun} is now available for only {number} {plural noun}. This {second adjective} deal won’t last long, so {first verb} now!\n\nVisit our {third adjective} store, and for a limited time, get a free {second noun} with every purchase. Our {fourth adjective} team is ready to {second verb} you with any questions.\n\nHurry, this offer ends soon!",
     "Exciting Partnership Announcement!": "We are thrilled to announce a {first adjective} partnership between {first company name} and {second company name}. This collaboration will bring {second adjective} {first plural noun} and {third adjective} {second plural noun} to our customers.\n\nTogether, we will {verb} to achieve {first plural noun} and {second plural noun} to begin {present tense verb}!. We feel incredibly {emotion} to be partnering together!\n\nSincerely, The {first company name} and {second company name} Teams",
-    "Emergency Advisory Notice": "Attention all residents,\n\nDue to an unexpected {noun}, we have {past tense verb} all {first adjective} activities until further notice. Please ensure that your {second noun} is {present tense verb} at all times.\n\nFor your safety, avoid {second adjective} {third noun} and remember to end any {third adjective} situations immediately./n/nThank you for your cooperation and understanding during this {fourth adjective} time."
+    "Emergency Advisory Notice": "Attention all residents,\n\nDue to an unexpected {noun}, we have {past tense verb} all {first adjective} activities until further notice. Please ensure that your {second noun} is {present tense verb} at all times.\n\nFor your safety, avoid {second adjective} {third noun} and remember to end any {third adjective} situations immediately./n/nThank you for your cooperation and understanding during this {fourth adjective} time,\n\n{name} "
                       }
 
 #dictonary needed word and response going from empty to response filled
 Advertisment_List = {'first adjective': '', 'first noun': '', 'number': '', 'plural noun': '', 'second adjective': '', 'first verb': '', 'third adjective': '', 'second noun': '', 'fourth adjective': '', 'second verb': ''}
 Announcment_List = {'first adjective': '', 'first company name': '', 'second company name': '', 'second adjective': '', 'first plural noun': '', 'third adjective': '', 'second plural noun': '', 'verb': '', 'present tense verb': '', 'emotion': ''}
-Advisory_List = {'first noun': '', 'past tense verb': '', 'first adjective': '', 'second noun': '', 'present tense verb': '', 'second adjective': '', 'third noun': '', 'third adjective': '', 'fourth adjective': ''}
+Advisory_List = {'first noun': '', 'past tense verb': '', 'first adjective': '', 'second noun': '', 'present tense verb': '', 'second adjective': '', 'third noun': '', 'third adjective': '', 'fourth adjective': '', 'name': ''}
 
 Play_Story = True
 while Play_Story == True:
@@ -30,7 +30,7 @@ while Play_Story == True:
             print("Your story is: " + Story)
             break
 
-    #Select the corrct dictonary of required words to be requested, determined by the previously selected story
+    #Select the correct dictionary of required words to be requested, determined by the previously selected story
     if Story == "Final Advertisement Script":
         BlankSpace_Type = Advertisment_List
     elif Story == "Exciting Partnership Announcement!":
